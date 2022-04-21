e624c2288b910888e88b6e2a2dfecfc1a37938fc
franklinVillegas <091236villegas@gmail.com>
2022-04-21 01:01:36 -0500

    siga

--- application/models/Model_CostosReferenciales.php
+++ application/models/Model_CostosReferenciales.php
@@ -6,9 +6,9 @@ class Model_CostosReferenciales extends CI_Model
     function lista()
     {
         $db_sedecentral = $this->load->database('SIGA_SEDECENTRAL', true);
-        $query=$db_sedecentral->query("select GRUPO_BIEN+CLASE_BIEN+FAMILIA_BIEN+ITEM_BIEN as codigo,NOMBRE_ITEM,PRECIO_COMPRA,um.ABREVIATURA,TIPO_BIEN from CATALOGO_BIEN_SERV cbs inner join UNIDAD_MEDIDA um
+        $query=$db_sedecentral->query("select GRUPO_BIEN+CLASE_BIEN+FAMILIA_BIEN+ITEM_BIEN as CODIGO,NOMBRE_ITEM,PRECIO_COMPRA,um.ABREVIATURA,TIPO_BIEN from CATALOGO_BIEN_SERV cbs inner join UNIDAD_MEDIDA um
         on cbs.UNIDAD_MEDIDA=um.UNIDAD_MEDIDA 
-        where cbs.ESTADO='A' and cbs.ESTADO_MEF='A' and cbs.PRECIO_COMPRA!=0 order by cbs.TIPO_BIEN,codigo");
+        where cbs.ESTADO='A' and cbs.ESTADO_MEF='A' and cbs.PRECIO_COMPRA!=0 order by cbs.TIPO_BIEN,CODIGO");
         return $query->result();
     }
 
