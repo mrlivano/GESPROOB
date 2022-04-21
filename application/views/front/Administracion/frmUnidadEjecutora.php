69050fcde3c645fd6272a4a2097e9b846b8d4395
Tenshi <64874044+Myriam555@users.noreply.github.com>
2022-04-21 07:45:23 -0500

    Update frmUnidadEjecutora.php

--- application/views/front/Administracion/frmUnidadEjecutora.php
+++ application/views/front/Administracion/frmUnidadEjecutora.php
@@ -397,7 +397,7 @@ x<div class="right_col" role="main">
                           <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Telefono<span class="required">*</span>
                           </label>
                           <div class="col-md-6 col-sm-6 col-xs-12">
-                            <input id="txtTelefono" name="txtTelefono" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" type="text" maxlength="9">
+                            <input id="txtTelefono" name="txtTelefono" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" type="text">
                           </div>
                         </div>
                         <div class="item form-group">
