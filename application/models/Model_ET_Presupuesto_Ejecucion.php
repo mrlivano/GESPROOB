6e9183cdcb65b298e27d0494dc7e0132b8d6f6a0
Tenshi <64874044+Myriam555@users.noreply.github.com>
2022-05-17 17:22:05 -0500

    costo

--- application/models/Model_ET_Presupuesto_Ejecucion.php
+++ application/models/Model_ET_Presupuesto_Ejecucion.php
@@ -34,7 +34,7 @@ class Model_ET_Presupuesto_Ejecucion extends CI_Model
         return $metaSubpresupuesto->result();
     }
     function totalSubpresupuesto($id){
-        $metaSubpresupuesto=$this->db->query("select sum(parcial) as sum from S10_META_PARTIDA where Id_Subpresupuesto='".$id."' ");
+        $sumSubpresupuesto=$this->db->query("select sum(parcial) as sum from S10_META_PARTIDA where Id_Subpresupuesto='".$id."' ");
         return $sumSubpresupuesto->result()[0]->sum;
     }
     function ListaPresupuestoEjecucion()
