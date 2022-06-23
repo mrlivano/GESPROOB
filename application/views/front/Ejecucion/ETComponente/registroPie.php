e0c82bd7d5a1787f1a9af661bc8108b7a8a39979
Tenshi <64874044+Myriam555@users.noreply.github.com>
2022-06-23 00:21:22 -0500

    function macro

--- application/views/front/Ejecucion/ETComponente/registroPie.php
+++ application/views/front/Ejecucion/ETComponente/registroPie.php
@@ -47,7 +47,7 @@
 									<option value="<?php $presupuesto->id_presupuesto_ej?>"><?=$presupuesto->desc_presupuesto_ej?></option>
 							<?php }?></select></td>
 							<td><input class="variable" id="variable<?=$cont?>" name="variable<?=$cont?>" type="text"><?=$value->descripcion?></td>
-							<td><input id="macro<?=$cont?>" name="macro<?=$cont?>" type="text"><?=$value->macro?></td>
+							<td><input id="macro<?=$cont?>" name="macro<?=$cont?>" type="text" onchange="obtenerMacro(this)"><?=$value->macro?></td>
 							<td><input id="gasto<?=$cont?>" name="gasto<?=$cont?>" type="checkbox"></td>
 							<td><input id="monto<?=$cont?>" name="monto<?=$cont?>" type="text"><?=$value->monto?></td>
 							<td><button >guardar</button></td>
@@ -76,12 +76,52 @@ function agregarFila(cont){
   document.getElementById("tablePresupuestos").insertRow(-1).innerHTML = '<?php  $cont=$cont+1; ?><td><select name="presupuestoEjecucion'+contador+'" id="presupuestoEjecucion'+contador+'">'+
 								'<?php foreach ($PresupuestoEjecucion as $key1 => $presupuesto) { ?>'+
 									'<option value="<?php $presupuesto->id_presupuesto_ej?>"><?=$presupuesto->desc_presupuesto_ej?></option>'+
-							'<?php }?></select></td>'+
+							'<?php }?>'+
+							'<option value="9999">SUBTOTAL</option>'+
+							'<option value="9999">COSTO TOTAL EJECUCION DE OBRA</option>'+
+							'<option value="9999">PRESUPUESTO TOTAL</option>'+
+							'</select></td>'+
 							'<td><input class="variable" id="variable'+contador+'" name="variable'+contador+'" type="text"></td>'+
-							'<td><input id="macro'+contador+'" name="macro'+contador+'" type="text"></td>'+
+							'<td><input id="macro'+contador+'" name="macro'+contador+'" type="text" onchange="obtenerMacro(this)"></td>'+
 							'<td><input id="gasto'+contador+'" name="gasto'+contador+'" type="checkbox"></td>'+
 							'<td><input id="monto'+contador+'" name="monto'+contador+'" type="text"></td>'+
 							'<td><button >guardar</button></td>'
 }
+
+function splitMulti(str, tokens){
+        var tempChar = tokens[0]; // We can use the first token as a temporary join character
+        for(var i = 1; i < tokens.length; i++)
+            str = str.split(tokens[i]).join(tempChar);
+        
+        str = str.split(tempChar);
+        return str;
+  }
+
+	function obtenerMacro(macro) {
+		const indexResp = macro.id.split('macro')[1];
+		try {
+			console.log(indexResp);
+			const arrayMacro=splitMulti(macro.value,['+', '-', '/', '*']);
+			const arrayVariable=document.querySelectorAll('.variable');
+			
+			arrayMacro.forEach(variable => {
+				if(variable != ''){
+					arrayVariable.forEach(variableFind => {
+						if(variableFind.value == variable){
+							const indexVariable = variableFind.id.split('variable')[1];
+							const monto = document.querySelector('#monto'+indexVariable).value;
+							window[variable] = Number(monto);
+						}
+					});
+				}
+			});
+			const res=eval(macro.value);
+			const inputMonto = document.querySelector('#monto'+indexResp).value = res;
+			console.log(res);
+		} catch (error) {
+			document.querySelector('#monto'+indexResp).value = 0;
+			console.log(0);
+		}
+	}
 </script>
 
