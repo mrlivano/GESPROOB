3deea0e4a2616db7b9b55804940c843a5dc1e762
Tenshi <64874044+Myriam555@users.noreply.github.com>
2022-09-20 15:36:20 -0500

    avance mensual

--- application/views/front/Ejecucion/AvanceMensual/fichaInforme.php
+++ application/views/front/Ejecucion/AvanceMensual/fichaInforme.php
@@ -123,10 +123,59 @@
 	$(function()
 	{
 		$('input').attr('autocomplete', 'off');		
+
+		$('#frmFichaInforme').formValidation(
+		{
+			framework: 'bootstrap',
+			excluded: [':disabled', ':hidden', ':not(:visible)', '[class*="notValidate"]'],
+			live: 'enabled',
+			message: '<b style="color: #9d9d9d;">Asegúrese que realmente no necesita este valor.</b>',
+			trigger: null,
+			fields:
+			{
+				txtAvanceFinanciero:
+			{
+				validators:
+				{
+					notEmpty:
+					{
+						message: '<b style="color: red;">El campo "Avance Financiero" es requerido.</b>'
+					},
+					regexp:
+					{
+						regexp: /(((\d{1,3},)(\d{3},)*\d{3})|(\d{1,3}))\.?\d{1,2}?$/,
+						message: '<b style="color: red;">El campo "Avance Financiero" debe ser un monto válido.</b>'
+					}
+				}
+			},
+			txtAvanceFisico:
+			{
+				validators:
+				{
+					notEmpty:
+					{
+						message: '<b style="color: red;">El campo "Avance Físico" es requerido.</b>'
+					},
+					regexp:
+					{
+						regexp: /^(0*100{1,1}\.?((?<=\.)0*)?%?$)|(^0*\d{0,2}\.?((?<=\.)\d*)?%?)$/,
+						message: '<b style="color: red;">El campo "Avance Físico" debe ser un porcentaje válido.</b>'
+					} 
+				}
+			}			
+			}
+		});
 	});
 
-    function guardarInformeMensual()
+  function guardarInformeMensual()
     {
+
+		event.preventDefault();
+    $('#frmFichaInforme').data('formValidation').validate();
+		if(!($('#frmFichaInforme').data('formValidation').isValid()))
+		{
+			return;
+		}
 		if($('#txtAvanceFisico').val().trim()=='')
 		{
 			swal('','El campo "Avance Físico" es requerido.','error');
